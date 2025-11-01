caa276e6d4bfead2de7d18e2a6ca84d2608bd86c
Anusri Mekala <166609758+Anu6304069658@users.noreply.github.com>
2025-11-01 19:57:23 +0530

    Add files via upload

---
+++ DataTypes1.java
@@ -0,0 +1,9 @@
+//Data Types and Operators - Assignment 1
+public class DataTypes1 {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        double radius = 4; 
+        double area = pi * radius * radius;
+        System.out.println(area);
+    }
+}
