a78ffb9adcb3bd4490c2a337c45f0a525434343f
Anusri Mekala <166609758+Anu6304069658@users.noreply.github.com>
2025-11-01 19:46:54 +0530

    Add files via upload

---
+++ Introduction.java
@@ -0,0 +1,9 @@
+//introduction to java- assignment 1
+public class Introduction {
+    	public static void main(String[] args) {
+		System.out.println("Door No: D089");
+        System.out.println("Street: St. Louis Street");
+        System.out.println("City: Springfield");
+        System.out.println("ZIP Code: 62729");
+	}
+}
