596afa555cb559ce457c5bcc1267378170267b8b
Anusri Mekala <166609758+Anu6304069658@users.noreply.github.com>
2025-11-01 19:50:33 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,10 @@
+public class DataTypes {
+    public static void main(String[] args) {
+		int principal=5000;
+        int rate=10;
+        int time=5;
+        //Simple Interest = (principal*rate of interest*time)/100 
+        double simpleInterest=(principal*rate*time)/100;
+        System.out.println(simpleInterest);
+	}
+}
