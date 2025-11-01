94405316f0c46553c64d8713fb70a0a37c26239f
Anusri Mekala <166609758+Anu6304069658@users.noreply.github.com>
2025-11-01 20:00:54 +0530

    Add files via upload

---
+++ DataType2.java
@@ -0,0 +1,8 @@
+//Data Types and Operators - Assignment 2
+public class DataType2 {
+    public static void main(String[] args) {
+        double fahrenheit = 32; // 
+        double celsius = ((fahrenheit - 32) / 9) * 5;
+        System.out.println(celsius);
+    }
+}
