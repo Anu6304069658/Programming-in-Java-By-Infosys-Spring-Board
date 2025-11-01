ae54213cd63c4f0b905f30156ad84298b09a7d74
Anusri Mekala <166609758+Anu6304069658@users.noreply.github.com>
2025-11-01 19:43:48 +0530

    Add files via upload

---
+++ Intro.java
@@ -0,0 +1,8 @@
+//Introduction to java-exercise1
+public class Intro {
+    public static void main(String[] args) {
+		// Implement your code here 
+        System.out.println("My name is Maria Jerome");
+        System.out.println("My designation is Systems Engineer");
+	}
+}
