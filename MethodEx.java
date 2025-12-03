14f986b5d280a850d6b1249a1c22d91cc6eb4e32
Anusri Mekala <166609758+Anu6304069658@users.noreply.github.com>
2025-12-03 15:53:29 +0530

    Add files via upload

---
+++ MethodEx.java
@@ -0,0 +1,14 @@
+class Calculator{
+    public double findAverage(int number1,int number2,int number3)
+    {
+        double avg=(number1+number2+number3)/3.0;
+        return Math.round(avg*100.0)/100.0;
+    }
+}
+public class MethodEx {
+    public static void main(String[] args) {
+        Calculator calculator=new Calculator();
+        double result=calculator.findAverage(12,8 , 15);
+        System.out.println(result);
+    }
+}
